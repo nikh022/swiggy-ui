6f0eeb6741de81ad2fe8ac3a87669b61bf96b95a
nikhil022 <niksocial2@gmail.com>
2025-08-06 21:13:37 +0530

    dynamic navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import FrontPage from "./pages/frontPage"
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import SearchPage from './pages/searchPage';
+import FrontPage from "./pages/frontPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SearchPage from "./pages/searchPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-       <Route path="/" element={ <FrontPage />} />
-       <Route path="/search" element={ <SearchPage />} />
-
+        <Route path="/" element={<FrontPage />} />
+        <Route path="/search" element={<SearchPage />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/homePage/navbar.jsx
+++ src/components/homePage/navbar.jsx
@@ -1,98 +1,104 @@
 import SwiggyLogo from "../../assets/navbar_icons/swiggy_logo.svg";
 import downSymbol from "../../assets/navbar_icons/chevron_down.svg";
-import corporateImg from "../../assets/navbar_icons/swiggy_corporate.svg";
-import searchSymbol from "../../assets/navbar_icons/search.svg";
-import offerSymbol from "../../assets/navbar_icons/offer.svg";
-import signinSymbol from "../../assets/navbar_icons/signin.svg";
-import helpSymbol from "../../assets/navbar_icons/help.svg";
-import cartSymbol from "../../assets/navbar_icons/cart.svg";
 import { useNavigate } from "react-router-dom";
+import {
+  Briefcase,
+  Search,
+  Tag,
+  HelpCircle,
+  User,
+  ShoppingCart,
+} from "lucide-react";
+
+const navItems = [
+  {
+    id: "corporate",
+    text: "Corporate",
+    icon: Briefcase,
+    path: null,
+  },
+  {
+    id: "search",
+    text: "Search",
+    icon: Search,
+    path: "/search",
+  },
+  {
+    id: "offers",
+    text: "Offers",
+    icon: Tag,
+    path: null,
+    badge: "NEW",
+  },
+  {
+    id: "help",
+    text: "Help",
+    icon: HelpCircle,
+    path: null,
+  },
+  {
+    id: "signIn",
+    text: "Sign In",
+    icon: User,
+    path: null,
+  },
+  {
+    id: "cart",
+    text: "Cart",
+    icon: ShoppingCart,
+    path: null,
+  },
+];
 
 export default function Navbar() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const handleClick1 = () => {
+    navigate("/");
+  };
 
-    const handleClick1 = () => {
-        navigate("/");
-    }
-    const handleClick2 = () => {
-        navigate("/search");
-    }
-    return (
-        <>
-            <div className="flex p-3 px-7 bg-white shadow-md sticky top-0 z-50 ">
-                <div className="flex w-1/3">
-                    <div onClick={handleClick1} className="w-1/5 pt-1 cursor-pointer">
-                        <img src={SwiggyLogo} />
-                    </div>
-                    <div
-                        className="flex w-4/5 p-4 text-sm"
-                    >
-                        <button className="pr-3 text-gray-700 font-bold underline">
-                            Other
-                        </button>
-                        <button className="text-gray-500">
-                            Bengluru, Karnataka, India
-                        </button>
-                        <img
-                            className="pl-2 pt-1 cursor-pointer"
-                            height={25}
-                            width={30}
-                            src={downSymbol}
-                        ></img>
-                    </div>
-                </div>
-                <div className="flex justify-between w-2/3 grid-cols-7 p-4 text-gray-700 font-semibold">
-                    <div></div>
-                    <div className="flex cursor-pointer hover:text-orange-600">
-                        <div className="pr-2 pt-1">
-                            <img height={21} width={21} src={corporateImg} />
-                        </div>
-                        <div>Swiggy Corporate</div>
-                    </div>
-                    <div onClick={handleClick2} className="flex cursor-pointer hover:text-orange-600">
-                        <div className="pr-2 pt-1">
-                            <img height={21} width={21} src={searchSymbol} />
-                        </div>
-                        <div>Search</div>
-                    </div>
-                    <div className="flex cursor-pointer hover:text-orange-600">
-                        <div className="pr-2 pt-1">
-                            <img height={21} width={21} src={offerSymbol} />
-                        </div>
-                        <div className="relative">
-                            Offers
-                            <span className="absolute top-0 right-0 transform  translate-x-6 -translate-y-1/2 bg-red-500 text-white text-xs font-semibold px-1 rounded-full">
-                                NEW
-                            </span>
-                        </div>
-                    </div>
-                    <div className="flex cursor-pointer hover:text-orange-600">
-                        <div className="pr-2 pt-1">
-                            <img height={21} width={21} src={helpSymbol} />
-                        </div>
-                        <div>Help</div>
-                    </div>
-                    <div
-                        className="flex items-center hover:text-orange-600 cursor-pointer"
-
-                    >
-                        <img
-                            src={signinSymbol}
-                            height={28}
-                            width={28}
-                            className="pr-2 pt-1"
-                        />
-                        <div>Sign In</div>
-                    </div>
-                    <div className="flex cursor-pointer hover:text-orange-600">
-                        <div className="pr-2 pt-1">
-                            <img height={21} width={21} src={cartSymbol} />
-                        </div>
-                        <div>Cart</div>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="flex p-3 px-7 bg-white shadow-md sticky top-0 z-50 ">
+        <div className="flex w-1/3">
+          <div onClick={handleClick1} className="w-1/5 pt-1 cursor-pointer">
+            <img src={SwiggyLogo} />
+          </div>
+          <div className="flex w-4/5 p-4 text-sm">
+            <button className="pr-3 text-gray-700 font-bold underline">
+              Other
+            </button>
+            <button className="text-gray-500">
+              Bengluru, Karnataka, India
+            </button>
+            <img
+              className="pl-2 pt-1 cursor-pointer"
+              height={25}
+              width={30}
+              src={downSymbol}
+            ></img>
+          </div>
+        </div>
+        <div className="flex flex-wrap justify-center md:justify-end w-full md:w-2/3 items-center text-gray-700 font-semibold md:space-x-8 space-x-4">
+          {navItems.map((item) => (
+            <div
+              key={item.id}
+              onClick={item.path ? () => navigate(item.path) : undefined}
+              className="flex items-center cursor-pointer hover:text-orange-600 transition-colors duration-200 p-2 rounded-md"
+            >
+              <item.icon className="w-5 h-5 mr-2" />
+              <div className="relative">
+                {item.text}
+                {item.badge && (
+                  <span className="absolute -top-2 right-0 transform translate-x-full -translate-y-1/2 bg-red-500 text-white text-xs font-semibold px-2 py-0.5 rounded-full shadow-md">
+                    {item.badge}
+                  </span>
+                )}
+              </div>
             </div>
-        </>
-    );
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
